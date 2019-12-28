625519504a1f5727112a69a2ce6bcfedef5b9bad
Angélina RIET <angelina.riet@yahoo.com>
2019-12-28 00:13:23 +0000

    quest040

---
+++ back/sql/init.sql
@@ -0,0 +1,13 @@
+/* import as: mysql -u root -p < init.sql */
+
+CREATE database homerodyssey;
+USE homerodyssey;
+
+CREATE TABLE users
+(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(90) UNIQUE,
+    password VARCHAR(90),
+    name VARCHAR(90),
+    lastname VARCHAR(90)
+);
